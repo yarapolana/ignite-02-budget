8c6cdaf4a3ac8c57cbeaa422028d1f8a93b7046f
Yara Polana <yara.polana@gmail.com>
2021-04-15 09:48:05 +0000

    finalize application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+import { ToastContainer } from 'react-toastify'
+
 import { Dashboard } from './pages/Dashboard'
 import { Header } from './components/Header'
+import { RootProvider } from './contexts'
 import { GlobalStyle } from './styles/global'
+import { NewTransactionModal } from './components/NewTransactionModal'
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isTransactionModalOpen, setIsTransactionModalOpen] = useState(false)
+
+  function handleOpenTransactionModal() {
+    setIsTransactionModalOpen(true)
+  }
+
+  function handleCloseTransactionModal() {
+    setIsTransactionModalOpen(false)
+  }
+
   return (
-    <>
-      <Header />
+    <RootProvider>
+      <Header onOpenTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isModalOpen={isTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
+      <ToastContainer />
       <GlobalStyle />
-    </>
-  );
+    </RootProvider>
+  )
 }
- 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import { makeServer } from './mirage'
+
+import "react-toastify/dist/ReactToastify.css"
+
+makeServer()
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
